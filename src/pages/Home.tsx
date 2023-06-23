0939a965bbcb688a5516639838a255371d6b63d2
SomashMargarita <maargaritasivceva@gmail.come>
2023-06-23 15:21:59 +0300

    [dev] add home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// import "./styles/homePageStyle.scss";
+import Logo from "../components/Logo";
+import { useNavigate } from "react-router-dom";
+import "./styles/homeStyle.scss";
 
 const Home: React.FC = () => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate("/");
+    };
     return (
         <div className="home-page-wraper">
-            <h1>Welcome</h1>
+            <div className="home-page-logo">
+                <Logo />
+            </div>
+            <div className="home-page-main">
+                <div className="home-page-info">
+                    <h1 className="home-page-title"> CONGRATULATIONS</h1>
+                    <img
+                        className="home-page-decor"
+                        src={process.env.PUBLIC_URL + "/img/Decor.png"}
+                        alt="decor"
+                    />
+                </div>
+                <p className="home-page-text">
+                    Now you are on the main page. Soon we will provide you with detailed feedback on
+                    the result of your work
+                </p>
+                <button className="home-page-button" onClick={handleClick}>
+                    Log Out
+                </button>
+                <img
+                    className="home-page-img"
+                    src={process.env.PUBLIC_URL + "/img/People.png"}
+                    alt="people"
+                />
+            </div>
         </div>
     );
 };
