d948a1efee6902f3ffb9d8aa5713b9646a500679
SomashMargarita <maargaritasivceva@gmail.come>
2023-06-26 15:37:18 +0300

    [dev] fix style

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,6 +9,7 @@ const Logo = () => {
                 height: "5rem",
                 top: "3rem",
                 left: "-6.1rem",
+                zIndex: 99999,
             }}
             src={process.env.PUBLIC_URL + "/img/Logo.png"}
             alt="logo"
